c7bc2cdcec13bf4d478fb69ae59c7f868c97724e
Nagendra Gouda <nagendragouda390@gmail.com>
2026-03-30 14:52:46 +0530

    Updated T20 match.py with corrected content

--- T20 match.py
+++ T20 match.py
@@ -4,7 +4,7 @@ team1 = ' England '
 team2 = ' Australia '
 
 ch = [0,1,2,3,4,6,'wd','b','lb','nb','w']
-prob = [0.4,0.5,0.2,0.01,0.1,0.09,0.05,0.01,0.01,0.03,0.04]
+prob = [0.30,0.35,0.10,0.05,0.08,0.05,0.03,0.01,0.01,0.01,0.01]
 
 ball1 = 0
 team1_run = 0
@@ -40,9 +40,9 @@ while ball2 < 120:
         ball2 += 1
         team2_run += run2
     # to stop innings after all out or Target chased
-    if team2_wicket ==10 or team2_run > team1_run:
+    if team2_wicket == 10 or team2_run >= team1_run:
         break
-   
+
 print(f'{team2} : {team2_run}-{team2_wicket}({ball2//6}.{ball2%6})')
 print()
 print()
@@ -50,6 +50,6 @@ print()
 if team1_run > team2_run:
     print(f'{team1} won by {team1_run - team2_run} runs ')
 elif team2_run > team1_run:
-    print(f'{team2} won by {10- team2_wicket} wickets ')
+    print(f'{team2} won by {10 - team2_wicket} wickets ')
 else:
     print(f' Match tied!')
